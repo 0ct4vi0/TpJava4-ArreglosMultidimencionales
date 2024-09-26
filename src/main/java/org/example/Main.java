af6e6e2978b649e7098f1c539625f0f5b3aeb45d
Octavio Rolon <octaviorolonh@gmail.com>
2024-09-26 12:17:12 -0300

    ejercicio4 finalizado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,50 +6,81 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (true){
-            System.out.println("ingrese un nuemro mayor a 3 pero menor a 10");
+            System.out.println("ingrese un nuemro mayor a 4 pero menor a 10");
             int X= sc.nextInt();
-            if (X < 3 || X >10){
+            int [][] M1 = new int [X][X];
+            if (X < 4 || X >10){
                 System.out.println("numero ingresado es incorrecto, por favor intente otra vez");
             } else{
                 System.out.println("el numero esta dentro del rango,proseguimos con el ejercicio");
                 System.out.println();
-                System.out.println("completando la matriz:");
-                int [][] M1 = new int [X][X];
-                for (int i = 0; i < X; i++) {
-                    for (int j = 0; j < X; j++) {
-                        System.out.print("Posicion: " + i + ", " + j + ": ");
-                        M1[i][j] = sc.nextInt();
+                while (true){
+                    System.out.println("Porfavor, elija una de las opciones: ");
+                    System.out.println(" 1) Rellenar la matriz de números(OBLIGATORIO) " +
+                                        "2) Suma de una fila de la matriz " +
+                                        "3) Suma de una columna de la matriz " +
+                                        "4) Sumar la diagonal principal " +
+                                        "5) Sumar la diagonal inversa " +
+                                        "6) La media de todos los valores de la matriz)"+
+                                        "7) Salir");
+                    int option= sc.nextInt();
+                    if (option==7){
+                        System.out.println("gracias por participar");
+                        break;
+                    }else if (option!=1){
+                        System.out.println("primero tiene que rellenar el array primero");
+                    }else{
+                        switch (option){
+                            case 1:
+                                for (int i = 0; i < X; i++) {
+                                    for (int j = 0; j < X; j++) {
+                                        System.out.print("Posicion: " + i + ", " + j + ": ");
+                                        M1[i][j] = sc.nextInt();
+                                    }
+                                }
+                            case 2:
+                                System.out.println("ingrese la fila que desea sumar");
+                                int i = sc.nextInt();
+                                int suma = 0;
+                                    for (int j = 0; j < X; j++) {
+                                         suma += M1[i][j];
+                                    }
+                                System.out.println("La suma de la fila " + i + " es: " + suma);
+                            case 3:
+                                System.out.println("ingrese la columna que desea sumar");
+                                int j = sc.nextInt();
+                                suma = 0;
+                                for ( i = 0; i < X; i++) {
+                                    suma += M1[i][j];
+                                }
+                                System.out.println("La suma de la fila " + j + " es: " + suma);
+                            case 4:
+                                suma = 0;
+                                for (i = 0; i < X; i++) {
+                                    suma += M1[i][i];
+                                }
+                                System.out.println("La suma de la diagonal principal es: " + suma);
+                            case 5:
+                                 suma = 0;
+                                for (i = 0; i < X; i++) {
+                                    suma += M1[i][X-1-i];
+                                }
+                                System.out.println("La suma de la diagonal principal es: " + suma);
+                            case 6:
+                                int total = X * X;
+                                suma=0;
+                                for (i = 0; i<X; i++) {
+                                    for (j = 0; j < X; j++) {
+                                        suma += M1[i][X - 1 - i];
+                                    }
+                                }
+                                double media = suma/total;
+                                System.out.println("la media de los valores de la matriz es: "+media);
+                        }
                     }
                 }
-                System.out.println();
-                System.out.println("nuestra matiz es: ");
-                for (int i = 0; i < X; i++) {
-                    for (int j = 0; j < X; j++) {
-                        System.out.print(M1[i][j]);
-                    }
-                }
-                System.out.println();
-                int suma=0;
-                int []sumaAr = new int[X];
-                for (int i = 0; i < X; i++) {
-                    for (int j = 0; j < X; j++) {
-                        suma+=M1[i][j];
-                    }
-                }
-                for (int i = 0; i < X; i++) {
-                    for (int j = 0; j < X; j++) {
-                        sumaAr[i]=suma;
-                    }
-                }
-                System.out.println("la suma de las columnas de la matriz es: ");
-                for (int i = 0; i < X; i++) {
-                    for (int j = 0; j < X; j++) {
-                        System.out.print(sumaAr[i]);
-                    }
-                }
-                break;
             }
+            break;
         }
     }
 }
-
