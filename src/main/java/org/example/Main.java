4ba9d3d1334a72fefc76f2cc4564c1005f7bf84a
Octavio Rolon <octaviorolonh@gmail.com>
2024-10-09 14:05:14 -0300

    ejercicio5 en proceso

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,86 +1,103 @@
 package org.example;
+import javax.swing.*;
 import java.util.Scanner;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+    static String[][] golosinas = {
+            {"KitKat", "32", "10"},
+            {"Chicles", "2", "50"},
+            {"Caramelos de Menta", "2", "50"},
+            {"Huevo Kinder", "25", "10"},
+            {"Chetoos", "30", "10"},
+            {"Twix", "26", "10"},
+            {"M&M'S", "35", "10"},
+            {"Papas Lays", "40", "20"},
+            {"Milkybar", "30", "10"},
+            {"Alfajor Tofi", "20", "15"},
+            {"Lata Coca", "50", "20"},
+            {"Chitos", "45", "10"}
+    };
+    static double montoTotal = 0;
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        while (true){
-            System.out.println("ingrese un nuemro mayor a 4 pero menor a 10");
-            int X= sc.nextInt();
-            int [][] M1 = new int [X][X];
-            if (X < 4 || X >10){
-                System.out.println("numero ingresado es incorrecto, por favor intente otra vez");
-            } else{
-                System.out.println("el numero esta dentro del rango,proseguimos con el ejercicio");
-                System.out.println();
-                while (true){
-                    System.out.println("Porfavor, elija una de las opciones: ");
-                    System.out.println(" 1) Rellenar la matriz de números(OBLIGATORIO) " +
-                                        "2) Suma de una fila de la matriz " +
-                                        "3) Suma de una columna de la matriz " +
-                                        "4) Sumar la diagonal principal " +
-                                        "5) Sumar la diagonal inversa " +
-                                        "6) La media de todos los valores de la matriz)"+
-                                        "7) Salir");
-                    int option= sc.nextInt();
-                    if (option==7){
-                        System.out.println("gracias por participar");
-                        break;
-                    }else if (option!=1){
-                        System.out.println("primero tiene que rellenar el array primero");
-                    }else{
-                        switch (option){
-                            case 1:
-                                for (int i = 0; i < X; i++) {
-                                    for (int j = 0; j < X; j++) {
-                                        System.out.print("Posicion: " + i + ", " + j + ": ");
-                                        M1[i][j] = sc.nextInt();
-                                    }
-                                }
-                            case 2:
-                                System.out.println("ingrese la fila que desea sumar");
-                                int i = sc.nextInt();
-                                int suma = 0;
-                                    for (int j = 0; j < X; j++) {
-                                         suma += M1[i][j];
-                                    }
-                                System.out.println("La suma de la fila " + i + " es: " + suma);
-                            case 3:
-                                System.out.println("ingrese la columna que desea sumar");
-                                int j = sc.nextInt();
-                                suma = 0;
-                                for ( i = 0; i < X; i++) {
-                                    suma += M1[i][j];
-                                }
-                                System.out.println("La suma de la fila " + j + " es: " + suma);
-                            case 4:
-                                suma = 0;
-                                for (i = 0; i < X; i++) {
-                                    suma += M1[i][i];
-                                }
-                                System.out.println("La suma de la diagonal principal es: " + suma);
-                            case 5:
-                                 suma = 0;
-                                for (i = 0; i < X; i++) {
-                                    suma += M1[i][X-1-i];
-                                }
-                                System.out.println("La suma de la diagonal principal es: " + suma);
-                            case 6:
-                                int total = X * X;
-                                suma=0;
-                                for (i = 0; i<X; i++) {
-                                    for (j = 0; j < X; j++) {
-                                        suma += M1[i][X - 1 - i];
-                                    }
-                                }
-                                double media = suma/total;
-                                System.out.println("la media de los valores de la matriz es: "+media);
-                        }
-                    }
-                }
+        menuDeOpciones();
+    }
+
+    public static void menuDeOpciones() {
+        while (true) {
+            System.out.println("Porfavor, elija una de las opciones: ");
+            System.out.println("" +
+                    "a) Pedir una golosina " +
+                    "b) Mostrar golosinas " +
+                    "c) Rellenar golosinas " +
+                    "d) Salir");
+            String option = new Scanner(System.in).nextLine();
+            switch (option) {
+                case "a":
+                    pedirGolosina();
+                case "b":
+                    System.out.println("las golosinas que quedan son: ");
+                    mostrarGolosina();
+                case "c":
+                    rellenarGolosina();
+                case "d":
+                    System.out.println("gracias por utilizar nuestra maquina");
+                    System.out.println("la cantidad de ventas fueron: " + montoTotal);
+                    break;
             }
-            break;
         }
     }
+
+//Metodo para PEDIR LAS GOLOSINAS----------------
+
+    public static void pedirGolosina() {
+        mostrarGolosina();
+        System.out.println();
+        System.out.println("ingrese la posicion de la golosina");
+        int codigo = new Scanner(System.in).nextInt();
+        if (codigoIncorrecto(codigo)) {
+            System.out.println("Error, el codigo ingresado es incorrecto");
+            pedirGolosina();
+        }
+        int stickActual = Integer.parseInt(golosinas[codigo - 1][2]);
+        if (stickActual > 0) {
+            golosinas[codigo - 1][2] = Integer.toString(stickActual - 1);
+            System.out.println("gracias por su compra" + golosinas[codigo - 1][0] + ".");
+            montoTotal+= Double.parseDouble( golosinas[codigo - 1][1]);
+            menuDeOpciones();
+        }else {
+            System.out.println("la golosina "+ golosinas[codigo - 1][0]+"no posee stock, pro favor seleccione otra golosina");
+            pedirGolosina();
+        }
+    }
+
+    //   Metodo para mostrar las golosinas ----------------
+    public static void mostrarGolosina() {
+        System.out.println();
+        for (int i = 0; i < golosinas.length; i++) {
+            System.out.println((i + 1) + "\t" + completarConEspacios(golosinas[i][0]) + "\t" + "$" + golosinas[i][i] + "unidades");
+            System.out.println("-----------------------------");
+        }
+        System.out.println("----------------");
+    }
+
+    //  Metodo Para completar con ESPACIOS ----------------
+    private static String completarConEspacios(String cadena) {
+        int cantidadDeEspacios = 25 - cadena.length();
+        for (int i = 0; i < cantidadDeEspacios; i++) {
+            cadena += (" ");
+        }
+        return cadena;
+    }
+
+    // Metodo de verificacion de CODIGO
+    private static boolean codigoIncorrecto(int codigo) {
+        if (codigo < 1 || codigo > 12) {
+            return false;
+        }
+        return true;
+    }
 }
+
+
+
+
